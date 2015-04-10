ad23c87cc2d7a46ad2795b4ed4cb8eee4821c129
rubenjimenezarias <rubenjimenezarias@gmail.com>
2015-04-10 12:38:41 +0200

    modificado metodo goRoom en una linea

--- Game.java
+++ Game.java
@@ -146,22 +146,7 @@ public class Game
         String direction = command.getSecondWord();
 
         // Try to leave current room.
-        Room nextRoom = null;
-        if(direction.equals("north")) {
-            nextRoom = currentRoom.getExit("north");
-        }
-        if(direction.equals("east")) {
-            nextRoom = currentRoom.getExit("east");
-        }
-        if(direction.equals("south")) {
-            nextRoom = currentRoom.getExit("south");
-        }
-        if(direction.equals("west")) {
-            nextRoom = currentRoom.getExit("west");
-        }
-        if(direction.equals("southeast")) {
-            nextRoom = currentRoom.getExit("southeast");
-        }
+        Room nextRoom = currentRoom.getExit(direction);
 
         if (nextRoom == null) {
             System.out.println("There is no door!");

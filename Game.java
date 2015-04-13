2a374277c756a01606a5642148723799dbdaa263
rubenjimenezarias <rubenjimenezarias@gmail.com>
2015-04-13 08:57:11 +0200

    introducido comando look 0115

--- Game.java
+++ Game.java
@@ -122,6 +122,9 @@ public class Game
         else if (commandWord.equals("quit")) {
             wantToQuit = quit(command);
         }
+        else if (commandWord.equals("look")) {
+            printLocationInfo();
+        }
 
         return wantToQuit;
     }
